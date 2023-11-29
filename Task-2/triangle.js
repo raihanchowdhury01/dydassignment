4778e83e65edd1084d4ebb1ad614771836ea3955
raihanchowdhury01 <rakibc698@gmail.com>
2023-11-29 12:17:15 +0600

    second

--- Task-2/triangle.js
+++ Task-2/triangle.js
@@ -1,3 +1,3 @@
 const b = parseFloat(prompt("Input b of triangle number : "));
 const h = parseFloat(prompt("Input h of triangle number : "));
-document.getElementById('triangle').innerHTML = b * h / 2;
+document.getElementById('triangle').innerHTML = "triangle Number: " + b * h / 2;
--- Task-3/degree.js
+++ Task-3/degree.js
@@ -1,4 +1,4 @@
 const fahrenheit = parseFloat(prompt("Input Fahrenheit: "));
 const sm = 5 / 9;
 const centrigrate = (fahrenheit-32) * sm;
-document.getElementById('fahranhit').innerHTML = parseInt(centrigrate) + `<sup>o</sup>`;
+document.getElementById('fahranhit').innerHTML = "degree number" + parseInt(centrigrate) + `<sup>o</sup>`;
